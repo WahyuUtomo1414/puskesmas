c157390de785dce1289cbc7b84caff3b205e9c1e
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-11 20:38:47 +0700

    buat resourec setting

---
+++ app/Filament/Resources/Settings/Schemas/SettingForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Settings\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class SettingForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('key')
+                    ->required(),
+                Textarea::make('value')
+                    ->required()
+                    ->columnSpanFull(),
+                Toggle::make('active')
+                    ->required(),
+                TextInput::make('created_by')
+                    ->required()
+                    ->numeric()
+                    ->default(1),
+                TextInput::make('updated_by')
+                    ->numeric(),
+                TextInput::make('deleted_by')
+                    ->numeric(),
+            ]);
+    }
+}
