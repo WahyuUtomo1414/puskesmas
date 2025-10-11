0fc37f1585b64eeaa6117f86b9ad5c11d6bf16af
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-11 20:54:24 +0700

    buat resource category

---
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class CategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                Toggle::make('active')
+                    ->required(),
+                TextInput::make('created_by')
+                    ->required()
+                    ->numeric()
+                    ->default(1),
+                TextInput::make('updated_by')
+                    ->numeric(),
+                TextInput::make('deleted_by')
+                    ->numeric(),
+            ]);
+    }
+}
