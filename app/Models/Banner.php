d2d88ef4c08970ce0a6a73e37d2bd75af1800c4e
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-11 19:29:27 +0700

    buat table banner

---
+++ app/Models/Banner.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\AuditedBySoftDelete;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Banner extends Model
+{
+    use HasFactory, Notifiable, AuditedBySoftDelete, SoftDeletes;
+    protected $table = 'benner';
+    protected $guarded = ['id'];
+
+    protected static function booted()
+    {
+        static::saved(function ($benner) {
+            Cache::forget("benner_image_{$benner->name}");
+        });
+
+        static::created(function ($benner) {
+            Cache::forget("benner_image_{$benner->name}");
+        });
+
+        static::updated(function ($benner) {
+            Cache::forget("benner_image_{$benner->name}");
+        });
+
+        static::deleted(function ($benner) {
+            Cache::forget("benner_image_{$benner->name}");
+        });
+    }
+}
---
+++ database/migrations/2025_10_11_122553_create_banners_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use App\Traits\BaseModelSoftDeleteDefault;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    use BaseModelSoftDeleteDefault;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banner', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->string('position', 128)->nullable();
+            $table->string('image', 128);
+            $this->base($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banner');
+    }
+};
