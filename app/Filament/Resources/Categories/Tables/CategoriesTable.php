a7ecc6468ac41d9fad6756d44bab5277a1267d79
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-11 20:56:42 +0700

    update category

--- app/Filament/Resources/Categories/Tables/CategoriesTable.php
+++ app/Filament/Resources/Categories/Tables/CategoriesTable.php
@@ -22,15 +22,12 @@ class CategoriesTable
                     ->searchable(),
                 IconColumn::make('active')
                     ->boolean(),
-                TextColumn::make('created_by')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('updated_by')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('deleted_by')
-                    ->numeric()
-                    ->sortable(),
+                TextColumn::make('createdBy.name')
+                    ->label('Created By'),
+                TextColumn::make('updatedBy.name')
+                    ->label("Updated by"),
+                TextColumn::make('deletedBy.name')
+                    ->label("Deleted by"),
                 TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
