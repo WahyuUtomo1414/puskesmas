88ec96f72b9998ba0b7806c50eaf582937eeadc2
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-11 19:37:30 +0700

    buat count table

---
+++ database/migrations/2025_10_11_123557_create_counts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Traits\BaseModelSoftDeleteDefault;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use BaseModelSoftDeleteDefault;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('count', function (Blueprint $table) {
+            $table->id();
+            $table->string('type', 128);
+            $table->string('name', 128);
+            $table->integer('value');
+            $this->base($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('count');
+    }
+};
