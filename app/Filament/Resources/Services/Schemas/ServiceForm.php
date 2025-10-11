63b31b6826cbcf3bd789d66de2b9e84dbd01078b
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-11 21:16:21 +0700

    buat resource service

---
+++ app/Filament/Resources/Services/Schemas/ServiceForm.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Resources\Services\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class ServiceForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                TextInput::make('schedule')
+                    ->required(),
+                FileUpload::make('image')
+                    ->image()
+                    ->required(),
+                TextInput::make('icon')
+                    ->required(),
+                TextInput::make('banner')
+                    ->required(),
+                Toggle::make('active')
+                    ->required(),
+                TextInput::make('created_by')
+                    ->required()
+                    ->numeric()
+                    ->default(1),
+                TextInput::make('updated_by')
+                    ->numeric(),
+                TextInput::make('deleted_by')
+                    ->numeric(),
+            ]);
+    }
+}
