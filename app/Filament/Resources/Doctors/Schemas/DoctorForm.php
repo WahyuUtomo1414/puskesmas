8c171ddd8a5b4e7ea04a34ad306925d271921c37
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-11 21:31:04 +0700

    buat resource doctor

---
+++ app/Filament/Resources/Doctors/Schemas/DoctorForm.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Resources\Doctors\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class DoctorForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('spesialist')
+                    ->required(),
+                FileUpload::make('image')
+                    ->image()
+                    ->required(),
+                TextInput::make('schedule')
+                    ->required(),
+                Toggle::make('active')
+                    ->required(),
+                TextInput::make('created_by')
+                    ->required()
+                    ->numeric()
+                    ->default(1),
+                TextInput::make('updated_by')
+                    ->numeric(),
+                TextInput::make('deleted_by')
+                    ->numeric(),
+            ]);
+    }
+}
