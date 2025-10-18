b262b5257379d571ec9b0c3ba6d627cf9aafb111
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-18 20:05:03 +0700

    buat profile controller

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('homepage');
 });
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/profile', [ProfileController::class, 'index']);
