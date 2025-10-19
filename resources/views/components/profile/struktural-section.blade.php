c0a04ef7d356dfbbfe13fcabc7059328397911ea
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-20 06:05:36 +0700

    tambah ilp

--- resources/views/components/profile/struktural-section.blade.php
+++ resources/views/components/profile/struktural-section.blade.php
@@ -16,5 +16,23 @@
                 Struktur Organisasi UPTD Puskesmas Purwodadi Tahun 2025
             </p>
         </div>
+
+
+        <!-- Struktur Organisasi -->
+        <div>
+            <h2 class="text-3xl font-bold text-indigo-700 mb-6 border-l-4 border-indigo-500 pl-3">
+                Alur Palayanan ILP
+            </h2>
+
+            <div class="flex justify-center py-4">
+                <img src="{{ asset('images/ilp.jpg') }}" 
+                    alt="Struktur Organisasi UPTD Puskesmas Purwodadi Tahun 2025"
+                    class="w-full max-w-5xl rounded-lg shadow-lg border border-gray-200">
+            </div>
+
+            <p class="text-gray-600 text-center mt-12 text-sm">
+                Alur Palayanan ILP
+            </p>
+        </div>
     </div>
 </section>
