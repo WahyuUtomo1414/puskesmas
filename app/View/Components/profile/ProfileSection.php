63b93a400cfa7e37340712b9021b4cca276c6d72
apekking28 <apekking28@gmail.com>
2025-10-22 04:26:08 +0700

    fix: banner duplicate section & create setting for wa,ig,tiktok

--- app/View/Components/profile/ProfileSection.php
+++ app/View/Components/profile/ProfileSection.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\View\Components\Home;
+namespace App\View\Components\Profile;
 
 use Closure;
 use Illuminate\Contracts\View\View;
@@ -8,21 +8,13 @@ use Illuminate\View\Component;
 
 class ProfileSection extends Component
 {
-    /**
-     * Create a new component instance.
-     */
     public $profileData;
 
-    public function __construct(
-        $profileData
-    )
+    public function __construct($profileData)
     {
         $this->profileData = $profileData;
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.profile.profile-section');
--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,65 +1,78 @@
 <footer class=" bg-[#1B3C53] opacity-100  !text-[#cacaca]">
-        <div class="container-fluid">
+    <div class="container-fluid">
         <footer class=" bg-[#1B3C53] opacity-100  !text-[#cacaca]">
             <div class="container pt-[4.5rem] xl:pt-[7rem] lg:pt-[7rem] md:pt-[7rem] pb-[1.75rem]">
-            <div class="flex flex-wrap mx-[-15px] !mt-[-30px] xl:!mt-0 lg:!mt-0">
-                
-                <div class="xl:w-4/12 lg:w-4/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
-                    <div class="widget">
-                        <h3 class="h2 !mb-3 !text-white !leading-[1.35]">UPDT Puskesmas Purwodadi</h3>
-                        <p class="lead !leading-[1.65] text-[0.9rem] font-medium !mb-5">Kami percaya bahwa kesehatan adalah investasi terbaik.</p>
-                    </div>
-                <!-- /.widget -->
-                </div>
+                <div class="flex flex-wrap mx-[-15px] !mt-[-30px] xl:!mt-0 lg:!mt-0">
 
-                <!-- /column -->
-                <div class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full lg:!ml-[16.66666667%] xl:!ml-[16.66666667%] max-lg:!mt-[30px]">
-                    <div class="widget">
-                        <h4 class="widget-title !text-white !mb-3">Sosial Media</h4>
-                        <ul class="pl-0 list-none text-inherit  !mb-0">
-                            <li><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="{{ $instagram }}" target="_blank">Instagram</a></li>
-                            <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="{{ $tiktok }}" target="_blank">Tiktok</a></li>
-                            <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="{{ $facebook }}" target="_blank">Facebook</a></li>
-                        </ul>
+                    <div
+                        class="xl:w-4/12 lg:w-4/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
+                        <div class="widget">
+                            <h3 class="h2 !mb-3 !text-white !leading-[1.35]">UPDT Puskesmas Purwodadi</h3>
+                            <p class="lead !leading-[1.65] text-[0.9rem] font-medium !mb-5">Kami percaya bahwa kesehatan
+                                adalah investasi terbaik.</p>
+                        </div>
+                        <!-- /.widget -->
                     </div>
-                <!-- /.widget -->
-                </div>
 
-                <!-- /column -->
-                <div class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full max-lg:!mt-[30px]">
-                    <div class="widget">
-                        <h4 class="widget-title !text-white !mb-3">Halaman</h4>
-                        <ul class="pl-0 list-none   !mb-0">
-                            <li><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Profile</a></li>
-                            <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Layanan</a></li>
-                            {{-- <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Pricing</a></li>
+                    <!-- /column -->
+                    <div
+                        class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full lg:!ml-[16.66666667%] xl:!ml-[16.66666667%] max-lg:!mt-[30px]">
+                        <div class="widget">
+                            <h4 class="widget-title !text-white !mb-3">Sosial Media</h4>
+                            <ul class="pl-0 list-none text-inherit  !mb-0">
+                                <li><a class="!text-[#cacaca] hover:!text-[#3f78e0]"
+                                        href="https://www.instagram.com/{{ $instagram }}"
+                                        target="_blank">Instagram</a></li>
+                                <li class="!mt-[0.35rem]"><a href="{{ 'https://www.tiktok.com/@' . $tiktok }}"
+                                        target="_blank">Tiktok</a>
+                                </li>
+                                <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]"
+                                        href="https://www.facebook.com/{{ $facebook }}" target="_blank">Facebook</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.widget -->
+                    </div>
+
+                    <!-- /column -->
+                    <div
+                        class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full max-lg:!mt-[30px]">
+                        <div class="widget">
+                            <h4 class="widget-title !text-white !mb-3">Halaman</h4>
+                            <ul class="pl-0 list-none   !mb-0">
+                                <li><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="/profile">Profile</a></li>
+                                <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]"
+                                        href="/service">Layanan</a></li>
+                                {{-- <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Pricing</a></li>
                             <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Features</a></li> --}}
-                        </ul>
+                            </ul>
+                        </div>
+                        <!-- /.widget -->
                     </div>
-                <!-- /.widget -->
+
+                    <!-- /column -->
+                    <div
+                        class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full max-lg:!mt-[30px]">
+                        <div class="widget">
+                            <h4 class="widget-title !text-white !mb-3">Kontak</h4>
+                            <address class=" not-italic !leading-[inherit] !mb-4">Karang Endah, Kec. Semendawai Suku
+                                III, Kabupaten Ogan Komering Ulu Timur, Sumatera Selatan</address>
+                            <a class="!text-[#cacaca] hover:!text-[#3f78e0]">085266430643</a>
+                        </div>
+                        <!-- /.widget -->
+                    </div>
+                    <!-- /column -->
+
+                </div>
+                <!--/.row -->
+
+                <hr class="!mt-13 xl:!mt-20 lg:!mt-20 md:!mt-20 !mb-7">
+                <div class="xl:!flex lg:!flex md:!flex items-center justify-between">
+                    <p class="!mb-2 xl:!mb-0 lg:!mb-0">© 2025 Puskesmas Purwodadi.</p>
+                    <p class="!mb-2 xl:!mb-0 lg:!mb-0">All rights reserved.</p>
                 </div>
 
-                <!-- /column -->
-                <div class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full max-lg:!mt-[30px]">
-                    <div class="widget">
-                        <h4 class="widget-title !text-white !mb-3">Kontak</h4>
-                        <address class=" not-italic !leading-[inherit] !mb-4">Karang Endah, Kec. Semendawai Suku III, Kabupaten Ogan Komering Ulu Timur, Sumatera Selatan</address>
-                        <a class="!text-[#cacaca] hover:!text-[#3f78e0]">085266430643</a>
-                    </div>
-                <!-- /.widget -->
-                </div>
-                <!-- /column -->
-
-            </div>
-            <!--/.row -->
-
-            <hr class="!mt-13 xl:!mt-20 lg:!mt-20 md:!mt-20 !mb-7">
-            <div class="xl:!flex lg:!flex md:!flex items-center justify-between">
-                <p class="!mb-2 xl:!mb-0 lg:!mb-0">© 2025 Puskesmas Purwodadi.</p>
-                <p class="!mb-2 xl:!mb-0 lg:!mb-0">All rights reserved.</p>
-            </div>
-
-            <!-- /.container -->
+                <!-- /.container -->
         </footer>
-        </div>
+    </div>
 </footer>
